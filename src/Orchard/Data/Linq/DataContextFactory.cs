a47190ba0558c4b172fad853971af53cc84db11e
hhland <hhlin54133@gmail.com>
2015-08-28 14:21:55 +0800

    add linq model

---
+++ src/Orchard/Data/Linq/DataContextFactory.cs
@@ -0,0 +1,42 @@
+﻿using DbLinq.Data.Linq;
+using DbLinq.MySql;
+using DbLinq.PostgreSql;
+using DbLinq.SqlServer;
+using MySql.Data.MySqlClient;
+using Orchard.Environment.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orchard.Data.Linq
+{
+    public class DataContextFactory
+    {
+
+        public static ShellSettings settings { get; set; }
+
+
+        
+
+        public static DataContext createDataContext(string name) {
+            DataContext dc = null;
+            string constr = settings.DataConnectionString;
+            switch (settings.DataProvider) {
+               case "sqlserver": { dc = new SqlServerDataContext(constr);break; }
+                case "mysql": { dc = new MySqlDataContext(new MySqlConnection(constr)); break; }
+                // case "post": { dc = new PgsqlDataContext(constr); break; }
+                default: break;
+            };
+            
+            return dc;
+        }
+
+        public static DataContext createDefaultDataContext() {
+            return createDataContext("default");
+        }
+
+    }
+}
